3cf589b07444a0c55bfd8fe4211e208653777dee
Menna-Ashraf512 <mennaalla@gmail.com>
2025-05-16 01:22:08 +0300

    changes pathes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -8,11 +8,13 @@ export const routes: Routes = [
     {path:'',redirectTo: 'home', pathMatch:'full'},
 
     {path:'',component:AuthLayoutComponent,canActivate:[logedGuard],children:[
+        {path:'' , redirectTo: 'login' , pathMatch:'full'},
         {path:"register",loadComponent: ()=> import('./core/pages/register/register.component').then(c => c.RegisterComponent)},
         {path:"login",loadComponent: ()=> import('./core/pages/login/login.component').then(c => c.LoginComponent)},
         {path:"forget-password",loadComponent: ()=> import('./core/pages/forget-password/forget-password.component').then(c => c.ForgetPasswordComponent)},
     ]},
     {path:'',component:BlankLayoutComponent ,canActivate:[authGuard],children:[
+        {path:'' , redirectTo: 'home' , pathMatch:'full'},
         {path:"home",loadComponent: ()=> import('./festures/pages/home/home.component').then(c => c.HomeComponent)},
         {path:"brands",loadComponent: ()=> import('./festures/pages/brands/brands.component').then(c => c.BrandsComponent)},
         {path:"products",loadComponent: ()=> import('./festures/pages/products/products.component').then(c => c.ProductsComponent)},
